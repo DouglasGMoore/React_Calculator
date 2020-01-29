36030098aa50df9b44e5a7e5d9e7956a14d4528e
douglasgmoore <douglasg.moore@gmail.com>
2020-01-29 13:40:40 -0600

    calc project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Screen from './components/screen';
+import Buttons from './components/buttons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      result:''
+    }
+
+  }
+  onClick= button =>{
+    if(button === '='){
+      this.calculate();
+    } else if( button ==='C'){
+      this.delete();
+    }else if(button === 'CE'){
+      this.reset();
+    }else {
+      this.setState({
+        result: this.state.result + button
+      })
+      
+    }
+  }
+ 
+  calculate=()=>{
+    try{
+      this.setState({
+        // eslint-disable-next-line no-eval
+        result: (eval(this.state.result) || '')+""
+      })
+    } catch (e){
+      this.setState({
+        result: 'error'
+      })
+    }
+    
+  };
+  reset=()=>{
+    this.setState({
+      result:''
+    })
+  };
+  delete=()=>{
+    this.setState({
+      result:this.state.result.slice(0,-1)
+    })
+  };
+  
+  render(){
+    return (
+      <div className="App">
+        <Screen result={this.state.result}/>
+        <Buttons onClick={this.onClick}/>
+      </div>
+    );
+  } 
 }
 
 export default App;
